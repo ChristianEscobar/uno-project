6f316168e96c9a342025a267c8279bd1d3d4dc95
enochwu <enochwu7@gmail.com>
2018-03-12 20:08:52 -0700

    working drag and js for login

--- app/public/assets/js/custom.js
+++ app/public/assets/js/custom.js
@@ -1,6 +1,10 @@
+$(document).ready(function() {
 
+  $(".button-collapse").sideNav();
 
+  dragula([document.getElementById('hand'), document.getElementById('discard')], {
+    revertOnSpill: true
 
-dragula([document.getElementById('left'), document.getElementById('right')], {
-  revertOnSpill: true
+  });
+
 });
