52b7680a20ab2ef78ce9cd575b74e8933d9cf8a5
Christian Escobar <crs.escobar@gmail.com>
2018-03-18 11:29:44 -0700

    prevent dragging from discard into player hand

--- app/public/assets/js/custom.js
+++ app/public/assets/js/custom.js
@@ -3,7 +3,17 @@ $(document).ready(function() {
   $(".button-collapse").sideNav();
 
   dragula([document.getElementById('hand'), document.getElementById('discard')], {
-    revertOnSpill: true
+    revertOnSpill: true,
+    accepts: function (el, target, source, sibling) {
+    	// Do not allow dragging from Discard and dropping into Player's hand
+    	const sourceId = $(source).attr("id");
+
+    	if(sourceId === 'discard') {
+    		return false;
+    	}
+
+    	return true;
+    }
   });
 
   function updateDiscard(event) {
